31a2fbc573bf6a0ce827f979f8d4c253903dd3f8
ahmed samir <ahmed.samir.farid@gmail.com>
2024-02-08 23:40:21 +0200

    init

---
+++ components/layout/logo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const localePath = useLocalePath();
+</script>
+
+<template>
+  <div>
+    <NuxtLink
+      class="inline-flex items-center gap-x-2 text-xl font-semibold dark:text-white"
+      :to="localePath('/')"
+    >
+      <img class="w-30 h-auto" src="/LogoFull.png" alt="Discover Nuxt 3" />
+    </NuxtLink>
+  </div>
+</template>
