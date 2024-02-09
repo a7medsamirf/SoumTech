1f1cff67ce6333385fab2a35bab4e88df916001d
ahmed samir <ahmed.samir.farid@gmail.com>
2024-02-09 14:39:37 +0200

    V 1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require("tailwindcss/colors");
-const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: [
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
     "./app.vue",
+    "./error.vue",
     './node_modules/preline/preline.js',
   ],
   theme: {
